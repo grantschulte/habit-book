5b87cd2d73483cc2b332949292de6d18010d1cc9
Grant Schulte <schulte.grant@gmail.com>
2020-12-15 14:51:57 -0600

    Unpack

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  color: green;
+`;
 
 function App() {
   return (
     <div className="App">
-
+      <Title>Sup</Title>
     </div>
   );
 }
