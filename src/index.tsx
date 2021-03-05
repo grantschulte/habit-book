41e924ca4de33cd856c3182a91510e6bffb3ec97
Grant Schulte <schulte.grant@gmail.com>
2021-03-05 10:44:33 -0600

    Add service workers

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import reportWebVitals from "./reportWebVitals";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 const render = () => {
   const App = require("./app/App").default;
@@ -26,3 +27,5 @@ if (process.env.NODE_ENV === "development" && module.hot) {
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+serviceWorkerRegistration.register();
